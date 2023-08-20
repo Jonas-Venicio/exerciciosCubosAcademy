83108d42f7946ec7bf0744437831a24aff43a3ff
Jonas Venicio <jonasvenicio4@gmail.com>
2023-08-20 20:24:30 -0300

    converterCelsiusparaFahrenheit

---
+++ exercicioCelsiusFahrenheit/convertendo-centigrados-fahrenheit.js
@@ -0,0 +1,11 @@
+//Convertendo Celsius para Fahrenheit
+const temperatura = [21, 32];
+let celsius = temperatura[0];
+let fahrenheit = temperatura[1];
+let escala = 1.8;
+let resultado = celsius * escala + fahrenheit;
+
+console.log(`Fahrenheit ${resultado}`);
+
+
+
